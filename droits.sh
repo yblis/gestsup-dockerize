01c06e87874fe4e7075b83c6bd410b7651d421d3
yblis <yblisss@yahoo.fr>
2022-10-04 16:55:17 +0200

    Delete droits.sh

--- droits.sh
+++
@@ -1,7 +0,0 @@
-find ./html/ -type d -exec chmod 750 {} \; 
-find ./html/ -type f -exec chmod 640 {} \;
-chmod 770 -R ./html/upload
-chmod 770 -R ./html/images/model
-chmod 770 -R ./html/backup
-chmod 770 -R ./html/_SQL
-chmod 660 ./html/connect.php
